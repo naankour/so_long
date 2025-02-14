27763e4b8c7a4060b0071aa59033a30b197ec73d
Naziha <aankour.naziha@gmail.com>
2025-02-14 18:50:54 +0100

    14/02

--- so_long.c
+++ so_long.c
@@ -12,6 +12,28 @@
 
 #include "so_long.h"
 
+void	is_ber(char	*file_path)
+{
+	int	i;
+
+	i = 0;
+	while (file_path[i] != '\0')
+		i++;
+	if (i <= 4)
+	{
+		ft_printf("Error : Invalid file. Extension must be '.ber'.\n");
+		return ;
+	}
+	if (file_path[i - 1] == 'r' && file_path[i - 2] == 'e'
+			&& file_path[i - 3] == 'b' && file_path[i - 4] == '.')
+			return ;
+	else
+	{
+		ft_printf("Error : Invalid file. Extension must be '.ber'.\n");
+		exit(1) ;
+	}
+}
+
 void	is_rectangle(t_game *game)
 {
 	int	i;
@@ -26,11 +48,102 @@ void	is_rectangle(t_game *game)
 	{
 		if (actual_lenght != game->map->grid_lenght)
 		{
-			ft_printf("Error : THe map must be rectangular.\n");
+			ft_printf("Error : The map must be rectangular.\n");
 			return ;
 		}
 		i++;
 	}
+}
+
+void	only_valid_chars(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (game->map->grid[i] != NULL)
+	{
+		j = 0;
+		while (game->map->grid[i][j] != '\0')
+		{
+			if (game->map->grid[i][j] != '0' && game->map->grid[i][j] != '1' &&
+				game->map->grid[i][j] != 'P' && game->map->grid[i][j] != 'E' &&
+				game->map->grid[i][j] != 'C')
+			{
+				ft_printf("Error : Map contains invalid characters.\n");
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+void	check_map_items(t_game *game)
+{
+	int	i;
+	int	j;
+	int	exit = 0;
+	int	player = 0;
+	int	collectible = 0;
+
+	i = 0;
+	exit = 0;
+	player = 0;
+	collectible = 0;
+	while (game->map->grid[i] != NULL)
+	{
+		j = 0;
+		while (game->map->grid[i][j] != '\0')
+		{
+			if(game->map->grid[i][j] == 'P')
+				player++;
+			else if(game->map->grid[i][j] == 'E')
+				exit++;
+			else if(game->map->grid[i][j] == 'C')
+				collectible++;
+			j++;
+		}
+		i++;
+	}
+	if ( player > 1 || player < 1 || exit > 1 || exit < 1 || collectible < 1)
+	{
+		ft_printf("Error : Map must have 1 'P', 1 'E', and at least 1 'C'.\n");
+		return ;
+	}
+}
+
+void	check_map_left_border(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	while (game->map->grid[i] != NULL)
+	{
+		if (game->map->grid[i][0] != '1')
+		{
+			ft_printf("Erroooooor\n");
+			return ;
+		}
+		i++;
+	}
+}
+
+void	check_map_right_border(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	while (game->map->grid[i] != NULL)
+		i++;
+	ft_printf("%d\n", i);
+	// {
+	// 	if (game->map->grid[i][ft_strlen()] != '1')
+	// 	{
+	// 		ft_printf("Erroooooor\n");
+	// 		return ;
+	// 	}
+	// 	i++;
+	// }
 }
 
 void	free_tab(char **tab)
@@ -72,10 +185,7 @@ void	read_map(const char *file_path, t_game *game)
 
 	fd = open(file_path, O_RDONLY);
 	if (fd < 0)
-	{
-		ft_printf("Error : Impossible d'ouvrir le fichier");
-		return ;
-	}
+		exit(1);
 	i = 0;
 	line = get_next_line(fd);
 	while (line != NULL)
@@ -98,7 +208,7 @@ void	read_map(const char *file_path, t_game *game)
 	fd = open(file_path, O_RDONLY);
 	if (fd < 0)
 	{
-		ft_printf("Error : Impossible d'ouvrir le fichier");
+		ft_printf("Error : Impossible d'ouvrir le fichier\n");
 		free_tab(game->map->grid);
 		return ;
 	}
@@ -106,7 +216,11 @@ void	read_map(const char *file_path, t_game *game)
 	line = get_next_line(fd);
 	while (line != NULL)
 	{
-		game->map->grid[i] = ft_substr(line, 0, (ft_strlen(line) - 1));
+		if( line[ft_strlen(line) - 1] == '\n')
+			game->map->grid[i] = ft_substr(line, 0, (ft_strlen(line) - 1));
+
+		else
+			game->map->grid[i] = ft_strdup(line);
 		i++;
 		free(line);
 		line = get_next_line(fd);
@@ -120,14 +234,20 @@ int	main(int ac, char **av)
 {
 	t_game	*game;
 
+	is_ber(av[1]);
 	game = malloc(sizeof(t_game));
 	game->ac = ac;
 	game->av = av;
 	read_map(av[1], game);
 	is_rectangle(game);
+	only_valid_chars(game);
+	check_map_items(game);
+	check_map_left_border(game);
+	check_map_right_border(game);
 	if (game->map)
 	{
 		free_tab(game->map->grid);
 		free(game->map);
 	}
 }
+
--- test.c
+++ test.c
@@ -1,127 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: naankour <naankour@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/10 14:09:23 by naankour          #+#    #+#             */
-/*   Updated: 2025/02/10 14:09:23 by naankour         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
 
-#include "so_long.h"
-
-
-// void	parsing2(t_game *game)
-// {
-// 	return ;
-// }
-
-// void	parsing(t_game *game)
-// {
-// 	parsing2(game);
-// }
-
-// int	main(int ac, char **av)
-// {
-// 	void	*mlx;
-// 	t_game	game;
-// 	char	**tab;
-
-// 	game.ac = ac;
-// 	game.av = av;
-// 	parsing(&game);
-// 	mlx = mlx_init();
-// 	printf("here\n");
-// }
-// typedef struct s_map
-// {
-// 	char	**grid;
-// 	int		lenght;
-// 	int		height;
-// }			t_map;
-
-// t_map
-
-// int	is_rectangle()
-// {
-// }
-void	free_tab(char **tab)
+void	is_ber(char	*file_path)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
+	while (file_path[i] != '\0')
 		i++;
-	}
-	free(tab);
-}
-void	read_map(const char *file_path)
-{
-	char	**grid;
-	int		i;
-	int		fd;
-	char	*line;
-
-	fd = open(file_path, O_RDONLY);
-	if(fd < 0)
+	if (i <= 4)
 	{
-		ft_printf("Error : Ipossible d'ouvrir le fichier");
-		return;
-	}
-	i = 0;
-	while((line = get_next_line(fd)) != NULL)
-	{
-		// ft_printf("%s", line);
-		i++;
-		free(line);
-	}
-	close(fd) ;
-
-	grid = malloc(sizeof(char *) * (i + 1));
-	if (!grid)
+		ft_printf("Error : Invalid file. Extension must be '.ber'.\n");
 		return ;
-
-	fd = open(file_path, O_RDONLY);
-	if(fd < 0)
-	{
-		ft_printf("Error : Ipossible d'ouvrir le fichier");
-		free_tab(grid);
-		return;
 	}
-
-	i = 0;
-	while((line = get_next_line(fd)) != NULL)
-	{
-		// grid[i] = malloc(sizeof(char) * ft_strlen(line));
-		// if (!grid[i])
-		// 	return ;
-		grid[i] = ft_substr(line, 0, (ft_strlen(line) - 1));
-		i++;
-		free(line);
-	}
-	close(fd);
-	i = 0;
-	while (grid[i])
-	{
-		int j = 0;
-		while (grid[i][j])
-		{
-			ft_printf("%c", grid[i][j]);
-			j++;
-		}
-		ft_printf("\n");
-		i++;
-	}
-	// free_tab(grid);
-	game.map->grid = grid;
+	if (file_path[i - 1] == 'r' && file_path[i - 2] == 'e'
+			&& file_path[i - 3] == 'b' && file_path[i - 4] == '.')
+			return ;
+	ft_printf("Error : Invalid file. Extension must be '.ber'.\n");
 }
-
-int	main(int ac, char **av)
+int	main()
 {
-	(void)ac;
-	read_map(av[1]);
+	char	*file_path = ".ber";
+	is_ber(file_path);
+	return (0);
 }
